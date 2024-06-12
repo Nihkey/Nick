b99ad63f8e6b54792dc94d2b40174af5e5e76b6d
Nihkey <nihkeymihles@gmail.com>
2024-06-11 21:57:32 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// I want to get my current age
+
+// Declare year of birth
+let yearOfBirth = 1997;
+
+// Declare current year
+let currentYear = 2024;
+
+// Get the difference between current year and my year of birth
+let currentAge = currentYear - yearOfBirth;
+
+// Display the result
+console.log(currentAge);
+
+// function to return the sum of two numbers
+// Declare a function with two parameters
+// return a statement for the sum of the two numbers
+// print the result of the function
+
+// Declare a function with two parameters
+const sumOfTwoNumbers = (num1, num2) => {
+  // return a statement for the sum of the two numbers
+  return num1 + num2;
+};
+// print the result of the function
+console.log(sumOfTwoNumbers(3, 5));
+
+// function to return product of two numbers
+// Declare function with two parameters
+function productOfTwoNumbs(num1, num2) {
+  // return a statement for the product of the parameters
+  return num1 * num2;
+}
+// print the result of the function
+console.log(productOfTwoNumbs(2, 2));
+
+// get the difference between two numbers
+
+// Declare a function that takes two numbers as params
+// return the difference between them
+// print the result
+
+const getDifference = (num1, num2) => {
+  return num1 - num2;
+};
+console.log(getDifference(10, 5));
+
+// TODO: get the remainder value between two numbers
+const getRemainder = (num1, num2) => {
+  return num1 % num2;
+};
+console.log(getRemainder(10, 3));
+
+// Declare function with two parameters
+// return a statement for the remainder of the two numbers
+// print the result of the function
